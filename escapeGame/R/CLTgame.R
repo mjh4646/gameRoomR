bd78244e67faf16446b5cd27975c56f4954a3a49
bradluen <bradluen@users.noreply.github.com>
2014-08-01 19:01:25 -0400

    Added switch graphic, changed initial state to blank data, improved printouts

--- escapeGame/R/CLTgame.R
+++ escapeGame/R/CLTgame.R
@@ -1,6 +1,6 @@
-#### This is the interpreter escape game package CLT game. 
-#### Created by Mike Higgins
-#### Modified by Brad Luen 7/31/14, 5:20pm
+#### Interpreter escape game package: The LLN room
+#### Mike Higgins & Brad Luen
+#### Last modified 8/1/14
 
 #### This function will eventually be used to 
 #### Update which location we are in.
@@ -33,8 +33,8 @@ initInterpreter = function(){
 	gameState = list()
 	gameState$Location = "FirstRoom"
 	gameState$Inventory = NULL
-	gameState$Data = rexp(100)
-	gameState$Means = mean(gameState$Data)
+	gameState$Data = c()
+	gameState$Means = c()
 	gameState$Switch = FALSE
 	
 	#Initialize Status
@@ -56,41 +56,47 @@ getLook = function(gameState, obj){
 				    3,3,17,17,NA,
 				    5,5,9,9,NA,
 				    11,11,16,16,11,NA,
+					13,13,13.75,13.75,13,NA,
+					13.75,13.75,14.5,14.5,13,NA,
 				    12,12,15.5,15.5)
 		myMap$y = c(0,4,4,0,0,NA,
 				    0,20,20,4,NA,
 				    0,20,20,4,NA,
 				    4,20,20,4,NA,
 				    4,15,15,4,NA,
-				    13,18,18,13,13,NA,
+				    15,18,18,15,15,NA,
+					11.5,12,12,11.5,11.5,NA,
+					11.5,12,12,11.5,11.5,NA,
 				    4,8,8,4)
 		myMap$range = c(0,20,0,20)
 		myMap$names = c("floor","leftWall","rightWall","backWall",
-			            "door","screen","safeFront")
+			            "door","screen","leftSwitch","rightSwitch","safeFront")
 		myCols =      c("tan", "lightblue","lightblue","lightblue",
-					    "white", "white","olivedrab")
+					    "white", "white","grey","black","olivedrab")
 		map(myMap, col = myCols, fill = TRUE)
 		points(c(8,8,14.75,14.75),c(9,9,5.75,5.75), pch = c(1,19,15,15),
 			cex = c(1.75,1.7,1.5,1), col = c("black","gold","white","black"))
             #points(c(12,13.5,15),c(15.5,14.5,17),pch = c("x","x","?"))
             #segments(11,14.5,16,16.5)
-		text(c(7,13.5,13.75),c(16,19,9),c("Door", "Screen", "Button"))
+		text(c(7,13.5,13.75,13.75),c(16,19,13,9),c("Door", "Screen", "Switch", "Button"))
 		
 		cat("In front of you, you see a Door, a Screen, a Switch, and a Button.\n")
 	}
 	
 	else if(obj == "screenFirstRoom"){
-        if(gameState$Switch==FALSE){
+		if(length(gameState$Data) == 0){
+			print("The screen is blank. I wonder what the button does?")
+        } else if(gameState$Switch==FALSE){
             hist(gameState$Data, main=paste("Sample number", length(gameState$Means)), xlab="")
             print(paste("The screen shows your current sample."))
         } else {
-            hist(gameState$Means, main="Sample means", xlab="Sample mean")
+            hist(gameState$Means, main=paste("Means of", length(gameState$Means), "samples"), xlab="Sample mean")
             print(paste("The screen shows the distribution of sample means."))
         }
 	}
 
 	else if(obj == "buttonFirstRoom"){
-		cat("There is a panel with one button on it.\nAn inscription reads: 'Press to generate a sample from an exponential distribution.'\nTo press the button, type use(\"Button\").")
+		cat("There is a panel with one button on it.\nAn inscription reads: 'Press to generate a sample from an exponential distribution.'\nTo press the button, type use(\"Button\").\n")
 	}
 	
 	else if(obj == "doorFirstRoom"){
@@ -105,10 +111,10 @@ getLook = function(gameState, obj){
 	else if(obj == "switchFirstRoom"){
         tempState = get("gameState", myE)
         if(tempState$Switch==FALSE){
-            cat("There is a switch with two settings: Data and Means.\nIt is currently set to Data.\nTo flick the switch, type use(\"Switch\").")
+            cat("There is a switch with two settings: Data and Means.\nIt is currently set to Data.\nTo flick the switch, type use(\"Switch\").\n")
         }
         if(tempState$Switch==TRUE){
-            cat("There is a switch with two settings: Data and Means.\nIt is currently set to Means.\nTo flick the switch, type use(\"Switch\").")
+            cat("There is a switch with two settings: Data and Means.\nIt is currently set to Means.\nTo flick the switch, type use(\"Switch\").\n")
         }
 	}
 	else{
@@ -120,14 +126,6 @@ getLook = function(gameState, obj){
 	}
 }
 
-unlock = function(gameState,number){
-    if(gameState$Password == number){
-    	print("The door opens, and you walk through. You are free. You're blinded by tears of joy, which prevent you from seeing the bus that crashes into you.")
-    }
-    else{
-    	print("A sad trombone plays. Try again.")
-    }
-}
 
 
 
@@ -144,7 +142,7 @@ getObjName = function(gameState,obj){
 			stop("Object in function must be in quotes.\nSee instructions() for details.")
 		}	
 		else{
-			print(obj)
+			#print(obj)
 			obj = tolower(obj)
 			name = paste(c(obj,gameState$Location),collapse = "")
 		}
@@ -156,7 +154,7 @@ useInterpreter = function(gameState,obj){
 	if(obj == "Button"){
         tempState = get("gameState", myE)
         tempState$Data = rexp(100)
-        print(paste("You pushed the button. One hundred data points fly across the room and into the screen. Their mean is", round(mean(tempState$Data),3),"."))
+        cat(paste("You pushed the button. \nOne hundred data points fly across the room and into the screen.\n Their mean is", round(mean(tempState$Data),3),".\n"))
         tempState$Means = c(tempState$Means, mean(tempState$Data))
         assign("gameState",tempState,myE)
         look("Screen")
@@ -179,7 +177,7 @@ useInterpreter = function(gameState,obj){
         } else if(length(tempState$Means) < 5){
             print("The door doesn't open. You're going to need a lot more samples.")
         } else if(shapiro.test(tempState$Means)$stat > 0.95){
-            print("The distribution of sample means is approximately normal. The door opens and you escape into the outside world. You win! (trumpets)")
+            cat("The distribution of sample means is approximately normal.\n The door opens and you escape into the outside world. You win! (trumpets)\n")
         } else {
             print("The door doesn't open. Try taking a few more samples.")
         }
